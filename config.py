988b5cc18393689634c315dcb1c474dee5068ba8
aksevad <aksevad@gmail.com>
2022-05-09 07:54:21 +0400

    First Release 0.7

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
-from starlette.config import Config
+# from starlette.config import Config
 #from starlette.datastructures import CommaSeparatedStrings, Secret
 
+BOT_TOKEN = "5336798046:AAHx9h_mhp-B7123cj26vGs4KFf-3aMcO8Y"
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-root_dir = dir_path
-config = Config(f'{root_dir}\.env')
-
-BOT_TOKEN = config('BOT_TOKEN', default=None)
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# root_dir = dir_path
+# config = Config(f'{root_dir}\.env')
+#
+# BOT_TOKEN = config('BOT_TOKEN', default=None)
